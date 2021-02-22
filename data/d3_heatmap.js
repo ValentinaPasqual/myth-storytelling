b17ee87d059573cbccb8cb1defc3e6ab0b62423a
ValentinaPasqual <valentina.pasqual@studio.unibo.it>
2021-02-22 17:54:17 +0100

    upload all

---
+++ data/d3_heatmap.js
@@ -0,0 +1,124 @@
+  // set the dimensions and margins of the graph
+  var margin = {top: 5, right: 25, bottom: 30, left: 60},
+    width = 500 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+  
+  // append the svg object to the body of the page
+  var svg = d3.select("#heatmap")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+  
+  //Read the data
+  d3.csv("data/cit_density.csv", function(data) {
+  
+    // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+    var myGroups = d3.map(data, function(d){return d.group;}).keys()
+    var myVars = d3.map(data, function(d){return d.variable;}).keys()
+  
+    // Build X scales and axis:
+    var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(myGroups)
+      .padding(0.05);
+    svg.append("g")
+      .style("font-size", 10)
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickSize(0))
+      .select(".domain").remove()
+  
+    // Build Y scales and axis:
+    var y = d3.scaleBand()
+      .range([ height, 0 ])
+      .domain(myVars)
+      .padding(0.05);
+    svg.append("g")
+      .style("font-size", 10)
+      .call(d3.axisLeft(y).tickSize(0))
+      .select(".domain").remove()
+  
+    // Build color scale
+    var myColor = d3.scaleSequential()
+      .interpolator(d3.interpolateReds)
+      .domain([1,700])
+  
+    // create a tooltip
+    var tooltip = d3.select("#heatmap")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px")
+      .style("max-width", "10rem")
+      .style("position", "absolute")
+    .style("z-index", "10")
+    .text("a simple tooltip");
+  
+    // Three function that change the tooltip when user hover / move / leave a cell
+    var mouseover = function(d) {
+      tooltip
+        .style("opacity", 1)
+      d3.select(this)
+        .style("stroke", "black")
+        .style("opacity", 1)
+    }
+    var mousemove = function(d) {
+      tooltip
+        .html("Aeneis lines " + d.variable + " of book " + d.group + ' has been cited in mythLOD dataset ' + d.value + " times, <br> representing the categories " + d.categories.split('@'))
+        .style("left", (d3.mouse(this)[0]+50) + "px")
+        .style("top", (d3.mouse(this)[1]-250) + "px")
+        .style("font-size", "0.8rem")
+
+    }
+    var mouseleave = function(d) {
+      tooltip
+        .style("opacity", 0)
+      d3.select(this)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+    }
+  
+    // add the squares
+    svg.selectAll()
+      .data(data, function(d) {return d.group+':'})
+      .enter()
+      .append("rect")
+        .attr("x", function(d) { return x(d.group) })
+        .attr("y", function(d) { return y(d.variable) })
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", x.bandwidth() )
+        .attr("height", y.bandwidth() )
+        .style("fill", function(d) { return myColor(d.value)} )
+        .style("stroke-width", 4)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave)
+  })
+  
+  // Add title to graph
+  svg.append("text")
+          .attr("x", 0)
+          .attr("y", -50)
+          .attr("text-anchor", "left")
+          .style("font-size", "0.7rem")
+          //.text("Aeneis citation distribution")
+          ;
+  
+  // Add subtitle to graph
+  svg.append("text")
+          .attr("x", 0)
+          .attr("y", 0)
+          .attr("text-anchor", "left")
+          .style("font-size", "1rem")
+          .style("fill", "grey")
+          .text("");
+  
---
+++ data/map/leaf-demo.js
@@ -0,0 +1,46 @@
+// See post: http://asmaloney.com/2015/06/code/clustering-markers-on-leaflet-maps
+
+var map = L.map( 'map', {
+  center: [10.0, 5.0],
+  minZoom: 2,
+  zoom: 2
+});
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+ subdomains: ['a','b','c']
+}).addTo( map );
+
+var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
+
+var myIcon = L.icon({
+  iconUrl: myURL + 'images/pin24.png',
+  iconRetinaUrl: myURL + 'images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+});
+
+var markerClusters = L.markerClusterGroup();
+
+for ( var i = 0; i < markers.length; ++i )
+{
+  var popup = '<div class="container"><div class="row"><div class="col-3"></div><div class="col-8"><img src=' + markers[i].img + ' width="100"></div><div class="col-3" ></div></div></div>' +
+              '<br/><b>Item Title:</b> ' + markers[i].title +
+              '<br/><b>Item URI:</b> '  + '<a href=' + markers[i].collocation + '>' + markers[i].item + '</a>' +
+              '<br/><b>Collocation Name:</b> ' + markers[i].label +
+              '<br/><b>Collocation URI:</b> ' + '<a href=' + markers[i].collocation + '>' + markers[i].collocation + '</a>';
+
+  var m = L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
+                  .bindPopup( popup );
+
+  markerClusters.addLayer( m );
+}
+
+map.addLayer( markerClusters );
+
+
+
+
+
+      
