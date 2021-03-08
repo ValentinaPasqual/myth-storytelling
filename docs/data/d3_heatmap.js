692a1f59af29dfd404923c7176c27d7c7203239e
ValentinaPasqual <valentina.pasqual@studio.unibo.it>
2021-03-08 10:44:30 +0100

    Update d3_heatmap.js

--- docs/data/d3_heatmap.js
+++ docs/data/d3_heatmap.js
@@ -4,7 +4,7 @@
     height = 500 - margin.top - margin.bottom;
   
   // append the svg object to the body of the page
-  var svg = d3.select("#heatmap")
+  var svg = d3version4.select("#heatmap")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -13,40 +13,40 @@
           "translate(" + margin.left + "," + margin.top + ")");
   
   //Read the data
-  d3.csv("data/cit_density.csv", function(data) {
+  d3version4.csv("data/cit_density.csv", function(data) {
   
     // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-    var myGroups = d3.map(data, function(d){return d.group;}).keys()
-    var myVars = d3.map(data, function(d){return d.variable;}).keys()
+    var myGroups = d3version4.map(data, function(d){return d.group;}).keys()
+    var myVars = d3version4.map(data, function(d){return d.variable;}).keys()
   
     // Build X scales and axis:
-    var x = d3.scaleBand()
+    var x = d3version4.scaleBand()
       .range([ 0, width ])
       .domain(myGroups)
       .padding(0.05);
     svg.append("g")
       .style("font-size", 10)
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).tickSize(0))
+      .call(d3version4.axisBottom(x).tickSize(0))
       .select(".domain").remove()
   
     // Build Y scales and axis:
-    var y = d3.scaleBand()
+    var y = d3version4.scaleBand()
       .range([ height, 0 ])
       .domain(myVars)
       .padding(0.05);
     svg.append("g")
       .style("font-size", 10)
-      .call(d3.axisLeft(y).tickSize(0))
+      .call(d3version4.axisLeft(y).tickSize(0))
       .select(".domain").remove()
   
     // Build color scale
-    var myColor = d3.scaleSequential()
-      .interpolator(d3.interpolateReds)
+    var myColor = d3version4.scaleSequential()
+      .interpolator(d3version4.interpolateReds)
       .domain([1,700])
   
     // create a tooltip
-    var tooltip = d3.select("#heatmap")
+    var tooltip = d3version4.select("#heatmap")
       .append("div")
       .style("opacity", 0)
       .attr("class", "tooltip")
@@ -64,22 +64,22 @@
     var mouseover = function(d) {
       tooltip
         .style("opacity", 1)
-      d3.select(this)
+      d3version4.select(this)
         .style("stroke", "black")
         .style("opacity", 1)
     }
     var mousemove = function(d) {
       tooltip
         .html("Aeneis lines " + d.variable + " of book " + d.group + ' has been cited in mythLOD dataset ' + d.value + " times, <br> representing the categories " + d.categories.split('@'))
-        .style("left", (d3.mouse(this)[0]+50) + "px")
-        .style("top", (d3.mouse(this)[1]-250) + "px")
+        .style("left", (d3version4.mouse(this)[0]+50) + "px")
+        .style("top", (d3version4.mouse(this)[1]-250) + "px")
         .style("font-size", "0.8rem")
 
     }
     var mouseleave = function(d) {
       tooltip
         .style("opacity", 0)
-      d3.select(this)
+      d3version4.select(this)
         .style("stroke", "none")
         .style("opacity", 0.8)
     }
